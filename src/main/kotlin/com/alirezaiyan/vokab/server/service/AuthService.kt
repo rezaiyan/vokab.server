c307ead252de4c4c867016787a4ddc9355170110
ali <alirezaiyann@gmail.com>
2025-10-21 19:07:27 +0200

    fix build error

--- src/main/kotlin/com/alirezaiyan/vokab/server/service/AuthService.kt
+++ src/main/kotlin/com/alirezaiyan/vokab/server/service/AuthService.kt
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
+import java.util.Date
 
 private val logger = KotlinLogging.logger {}
 
